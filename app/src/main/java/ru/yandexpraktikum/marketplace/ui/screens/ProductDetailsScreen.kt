f71b30bf6737464c564d607c4a836410454d8721
SAndreyV1683 <ya.sboevandrey@yandex.ru>
2025-04-05 22:16:59 +0300

    accessibility in progress

--- app/src/main/java/ru/yandexpraktikum/marketplace/ui/screens/ProductDetailsScreen.kt
+++ app/src/main/java/ru/yandexpraktikum/marketplace/ui/screens/ProductDetailsScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,6 +30,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import kotlinx.coroutines.launch
@@ -56,7 +58,7 @@ fun ProductDetailsScreen(
                 navigationIcon = {
                     IconButton(onClick = onNavigateBack) {
                         Icon(
-                            Icons.Default.ArrowBack,
+                            Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = stringResource(R.string.back)
                         )
                     }
@@ -72,12 +74,13 @@ fun ProductDetailsScreen(
         ) {
             Column(
                 modifier = Modifier
+                    .semantics(mergeDescendants = true) { }
                     .fillMaxSize()
                     .padding(16.dp)
             ) {
                 AsyncImage(
                     model = product.imageUrl,
-                    contentDescription = null,
+                    contentDescription = product.name,
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(300.dp),
@@ -107,7 +110,7 @@ fun ProductDetailsScreen(
                 
                 Spacer(modifier = Modifier.weight(1f))
 
-                // val buttonDescription = stringResource(R.string.add_product_to_cart, product.name)
+                val buttonDescription = stringResource(R.string.add_product_to_cart, product.name)
                 Button(
                     onClick = {
                         scope.launch {
@@ -118,6 +121,7 @@ fun ProductDetailsScreen(
                         }
                     },
                     modifier = Modifier.fillMaxWidth()
+                        .semantics { contentDescription = buttonDescription }
                 ) {
                     Icon(
                         Icons.Default.ShoppingCart,
